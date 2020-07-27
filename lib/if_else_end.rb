23374384cbb4c3bf78b6fed036d4469c1f5da43c
Haley Proctor <hproctor021@gmail.com>
2020-07-27 16:32:44 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,38 @@
-# Write your solution here
+#the_funk = true
+#if the_funk
+ # puts "We got the funk."
+#end
+#puts "Gotta have that funk."
+
+
+#chance_of_funk = 0.5
+#puts "Funk check, one two, one two."
+
+#if chance_of_funk >= 0.5
+ # puts "Pack ya bags, we on the move."
+#else
+#  puts "uhhh..."
+#end
+#puts "Clear a space for the cypha, in the room!"
+
+#chance_of_funk = 0.5
+#if chance_of_funk <= 0.15
+ # puts "C'mon and move ya feet!"
+#elsif (chance_of_funk > 0.15 && chance_of_funk < 0.49)
+#  puts "Just a little bit louder now."
+#else
+ # puts "Get funky now!"
+#end
+
+
+
+  
+#if Time.now.to_i % 2 == 0
+#current_time = Time.now
+#current_time = current_time.to_i
+
+if Time.now.to_i.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
